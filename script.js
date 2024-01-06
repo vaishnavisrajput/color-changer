d7c5a868c0ff880df624bbce10267c47f3b9a58c
Vaishnavi Singh Rajput <vaishnavirajput981@gmail.com>
2024-01-06 22:24:10 +0530

    Add all my files

---
+++ script.js
@@ -0,0 +1,34 @@
+const allButton = document.querySelectorAll('.button');
+const body = document.querySelector('body');
+
+allButton.forEach(function (button){
+    console.log(button)
+    button.addEventListener('click', function(event){
+        console.log(event)
+        console.log(event.target)
+        if(event.target.id === 'grey'){
+            body.style.backgroundColor = event.target.id
+        }
+        if(event.target.id === 'yellow'){
+            body.style.backgroundColor = event.target.id
+        }
+        if(event.target.id === 'blue'){
+            body.style.backgroundColor = event.target.id
+        }
+        if(event.target.id === 'red'){
+            body.style.backgroundColor = event.target.id
+        }
+        if(event.target.id === 'pink'){
+            body.style.backgroundColor = event.target.id
+        }
+        if(event.target.id === 'purple'){
+            body.style.backgroundColor = event.target.id
+        }
+        if(event.target.id === 'orange'){
+            body.style.backgroundColor = event.target.id
+        }
+        if(event.target.id === 'green'){
+            body.style.backgroundColor = event.target.id
+        }
+    })
+})
